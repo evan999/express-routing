b2be2f2e5cfe876f87931696a53a0eeae836af5f
Evan Meshberg <evanmeshberg.student@careerdevs.com>
2018-10-23 00:39:01 +0000

    Initialize express app

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const parseUrlencoded = bodyParser.urlenconded({extended: false});
 
 // GET route to display name on the page
 
-app.get('/:name', (request, response) => {
+app.get('/name/', (request, response) => {
     const name = 'Evan Meshberg';
     response.send(`<h1>${name}</h1>`);
 });
+
+
 
 const port = process.env.PORT || 5000;
 
