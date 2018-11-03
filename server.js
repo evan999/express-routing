4a39376f81ef03a2f715270e726e8b5bdd978d11
Evan Meshberg <evanmeshberg.student@careerdevs.com>
2018-11-03 23:56:10 +0000

    Clean up code

--- server.js
+++ server.js
@@ -10,8 +10,6 @@ app.use(bodyParser.json());
 
 let items = ['pumpkin', 'candy', 'autumn', 'mummy', 'witch', 'ghost'];
 
-
-
 // GET route to display name on the page
 
 app.get('/', (req, res) => {
@@ -75,11 +73,7 @@ app.delete('/api/item', (req, res) => {
     
     res.json(items);
 });
-/*
-app.delete('/api/item', (req, res) => {
-    
-});
-*/
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log('Listening on port', port));
